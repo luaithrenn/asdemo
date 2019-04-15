64914ee3d44202d76b0de64f0a24440fedaf59a0
luaithrenn <luaidemp@ie.ibm.com>
2019-04-15 11:34:40 +0100

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,25 @@
+PACKAGE_URL = 'PACKAGE_URL = 'git+https://41257bbce455a31902ccf02e455ccc7c3970b385@github.com/Luaithrenn/asdemo.git@''
+
+import numpy as np
+import pandas as pd
+from iotfunctions.preprocessor import BaseTransformer
+
+class MyCustomFunction(BaseTransformer):
+  '''
+  Multiple input item by 3.41
+  '''
+  url = PACKAGE_URL
+
+  def _init(self,
+    input_item,
+    output_item - 'output_item'
+    ):
+    self.input_item = input_item
+    self.output_item = output_item
+    super()._init_()
+
+  def execute(self, df):
+    df = df.copy()
+    df[self.output_item] = df[self.input_item]*3.41
+
+    return df
